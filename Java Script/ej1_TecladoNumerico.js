70f1b5217b5bed977c629af7336aa939c68057c6
anmol <anmolpreet.singh.7e8@itb.cat>
2025-01-08 12:38:10 +0100

      dia 8 de enero 2025

--- Java Script/ej1_TecladoNumerico.js
+++ Java Script/ej1_TecladoNumerico.js
@@ -1,39 +1,12 @@
-let divContenedor = document.getElementById('contenedor')
+const contenedor = document.getElementById("contenedor");
 
+for(let i = 1; i <= 9; i++){
 
-let num1 = document.createElement('div');
-num1.innerHtml = '1';
-num1.className = 'botonNumero';
-divContenedor.appendChild(num1)
+let botonTeclado = document.createElement('p'); //crear elemento
+botonTeclado.innerHTML = i; //añado el número
+botonTeclado.className = "botonNumero"; //le pongo la classe correspondiente
+contenedor.appendChild(botonTeclado) //añado el boton al div
 
-let num2 = document.createElement('div')
-let text2 = document.createTextNode('2');
-num2.appendChild(text2);
-       
-let num3 = document.createElement('div')
-let text3 = document.createTextNode('3');
-num3.appendChild(text3);
+botonTeclado.addEventListener('click'), () => handleKeyClick(key);
+}
 
-let num4 = document.createElement('div')
-let text4 = document.createTextNode('4');
-num4.appendChild(text4);
-
-let num5 = document.createElement('div')
-let text5 = document.createTextNode('5');
-num5.appendChild(text5);
-
-let num6 = document.createElement('div')
-let text6 = document.createTextNode('6');
-num6.appendChild(text6);
-
-let num7 = document.createElement('div')
-let text7 = document.createTextNode('7');
-num7.appendChild(text7);
-
-let num8 = document.createElement('div')
-let text8 = document.createTextNode('8');
-num8.appendChild(text8);
-
-let num9 = document.createElement('div')
-let text9 = document.createTextNode('9');
-num9.appendChild(text9);
